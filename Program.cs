3a78c0d865514f744ec028e03b9cf7a5a37819e3
SuhasBennur <suhasbennur@gmail.com>
2022-03-21 11:33:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.IO;
+class demo
+{
+    const string program = @"
+def sum(a, b):
+    return a+b
+";
+
+    public static void Main(string[] args)
+    {
+        ScriptEngine engine = Python.CreateEngine();
+
+        ScriptSource source = engine.CreateScriptSourceFromString(program);
+        ScriptScope scope = engine.CreateScope();
+
+        source.Execute(scope);
+
+        // get the function from the python side (remember functions are
+        // first class objects in python, you can refer to them like they 
+        // are variables)
+        var sumFunc = scope.GetVariable("sum");
+
+        var result = sumFunc(2, 2);
+
+        Console.WriteLine("result: {0}", result);
+    }
+}
